f1581d37e95238c56f64e14e4dc4aa63b1fff33e
Soumen Bhunia <sob99338@gmail.com>
2024-02-19 21:10:45 +0530

    router added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,55 @@
+import { BrowserRouter as Router, Route, Routes, Link, Outlet } from 'react-router-dom';
+import UserEntry from './auth/pages/UserEntry';
+import Login from './auth/pages/Login';
+import SignUp from './auth/pages/SignUp ';
+import StudentLandingLayout from './users/student/pages/StudentLandingLayout';
+import Dashboard from './users/student/pages/Dashboard';
+import HrLandingLayout from './users/hr/pages/HrLandingLayout';
+import HrHome from './users/hr/pages/HrHome';
+import AdminLandingLayout from './users/admin/pages/AdminLandingLayout';
+import AdminHome from './users/admin/pages/AdminHome';
+
+
 
 function App() {
   return (
     <>
-      <h1 className="bg-red-200 text-center ">welcome to this project</h1>
+   <Router>
+      <Routes>
+        {/* Entry Links */}
+        <Route path="/" element={<UserEntry/>} />
+        <Route path="/Login" element={<Login/>} />
+        <Route path="/SignUp" element={<SignUp/>} />
+
+        {/* Student Links */}
+        <Route
+          path="/Student"
+          element={<StudentLandingLayout />}
+        >
+          <Route path="/Student/" element={<Dashboard />} />
+          
+        </Route>
+
+        {/* HR Links */}
+        <Route path="/HR" 
+          element={<HrLandingLayout/>}
+        >
+          <Route path="/HR/" element={<HrHome />} />
+          
+        </Route>
+
+        {/* Admin Links */}
+        <Route path="/Admin" 
+          element={<AdminLandingLayout/>}
+        >
+           <Route path="/Admin/" element={<AdminHome/>} />
+
+        </Route>
+      </Routes>
+      
+      
+
+    </Router>
     </>
   );
 }
