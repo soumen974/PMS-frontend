61b73c773c9e54c18eeb917436a4ab461d065eb3
Soumen Bhunia <sob99338@gmail.com>
2024-04-01 16:05:36 +0530

    sign up and authentication layout added:

--- src/auth/pages/SignUp .jsx
+++ src/auth/pages/SignUp .jsx
@@ -1,7 +1,44 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Authentication from '../components/Authentication';
+import SignUppageUsedImg from "../assets/SignUppageUsedImg.svg";
+import InappLoader from "../../Loader/InappLoader";
 export default function SignUp () {
+
+  document.title = "New User..! [PlacementModule]";
+
+  const [isLoading,setisLoading]=useState(false);
+  
+  
+  useEffect(() => {
+    setisLoading(true);
+    setTimeout(()=>{
+    setisLoading(false)
+    },400)
+}, [])
+
+
+  const  EntryWayMessage_login="Already have an account";
+
   return (
-    <div>SignUp </div>
+    <>
+    
+    {
+      isLoading?
+      <>
+      <InappLoader/>
+      </>
+    
+    :
+      
+      <div className=" grid  place-content-center h-[100vh]  bg-gradient-to-r from-purple-700 via-pink-600 to-purple-400 ">
+      
+      <div className=' md:grid rounded xl:gap-3   bg-white grid-cols-[30%,70%] shadow-lg shadow-black-500/50 md:pl-4 p-10 md:p-0'>
+        <Authentication LinkButtonName={'SignUp'} Links={'/Login'} SignUpformUse={true} EntryWayMessage={EntryWayMessage_login} />
+        <img className='rounded-md hidden md:block md:pl-4 xl:pl-3' src={SignUppageUsedImg} alt="" />
+      </div>
+      
+      
+    </div>}
+     </>
   )
 }
--- src/auth/pages/UserEntry.jsx
+++ src/auth/pages/UserEntry.jsx
@@ -1,20 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Nav from '../components/Nav'
 import Section1 from '../components/Section1'
 import TopComp from '../components/TopComp'
 import PlacementGraphs from '../components/PlacementGraphs'
 import Faq from '../components/Faq'
 import Footer from '../components/Footer'
+import InappLoader from '../../Loader/InappLoader'
 
 export default function UserEntry() {
+
+  const [isLoading,setisLoading]=useState(false);
+  
+  
+  useEffect(() => {
+    setisLoading(true);
+    setTimeout(()=>{
+    setisLoading(false)
+    },400)
+}, [])
+
   return (
     <>
-      <Nav/>
-      <Section1/>
-      <TopComp/>
-      <PlacementGraphs/>
-      <Faq/>
-      <Footer/>
+
+    <div>
+      {
+        
+          isLoading?
+          <>
+          <InappLoader/>
+          </>
+        
+        :
+        <div>
+            <Nav/>
+            <Section1/>
+            <TopComp/>
+            <PlacementGraphs/>
+            <Faq/>
+            <Footer/>
+         </div>
+        
+      }
+      
+    </div>
+    
+      
 
     </>
   )
