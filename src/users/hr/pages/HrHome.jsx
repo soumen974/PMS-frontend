bd0dc3ed4149e426fac0e0da750ebf0681b927e3
Soumen Bhunia <sob99338@gmail.com>
2024-04-05 20:27:57 +0530

    working branch

--- src/users/hr/pages/HrHome.jsx
+++ src/users/hr/pages/HrHome.jsx
@@ -1,16 +1,7 @@
 import React from 'react'
-import JobPostComponentBox from '../components/JobPostComponentBox'
 
 export default function HrHome() {
   return (
-    <>
-<<<<<<< HEAD
-    make changesin home     
-=======
-       <div className='pr-[68rem]'>
-       
-      </div>    
->>>>>>> 7ba8c98 ( update)
-    </>
+    <div>HrHome</div>
   )
 }
