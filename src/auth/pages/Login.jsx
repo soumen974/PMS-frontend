194a808f484e3b903b4e2b9a7363992c4110d5ed
Soumen Bhunia <sob99338@gmail.com>
2024-04-10 21:27:49 +0530

    ui update Login page

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Authentication from '../components/Authentication';
-import LoginSideImage from "../assets/LoginPageUsedImg.svg";
+import LoginSideImage from "../assets/slide-1-pic.jpg";
 import InappLoader from "../../Loader/InappLoader";
 export default function Login() {
 
@@ -28,10 +28,10 @@ export default function Login() {
       :
 
       <div>
-        <div className=" grid  place-content-center h-[100vh]  bg-gradient-to-r from-purple-700 via-pink-600 to-purple-400 ">
+        <div className=" grid  place-content-center h-[100vh]   ">
           
           <div className=' md:grid rounded gap-3 bg-white grid-cols-[70%,30%] shadow-lg shadow-black-500/50 md:pr-8 p-10 md:p-0'>
-            <img className='rounded hidden md:block md:pr-2' src={LoginSideImage} alt="" />
+            <img className='rounded w-[50rem] md:pr-2' src={LoginSideImage} alt="" />
             <Authentication LinkButtonName={'Login'} Links={'/SignUp'} EntryWayMessage={EntryWayMessage_SignUp} />
 
           </div>
