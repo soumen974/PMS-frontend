39b01d6ba4678e2895f0ab37726f0adfc8098931
Rashmikant Pradhan <210301120110@cutm.ac.in>
2024-04-22 21:53:14 +0530

    modified student form page

--- src/users/hr/pages/Studentform.jsx
+++ src/users/hr/pages/Studentform.jsx
@@ -1,77 +1,116 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
 
 const StudentForm = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [branch, setBranch] = useState('');
-  const [campus, setCampus] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [middleName, setMiddleName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [branch, setBranch] = useState("");
+  const [campus, setCampus] = useState("");
+  const [school, setSchool] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const formData = {
-      name,
-      email,
-      branch,
-      campus
-    };
 
     try {
-      const response = await fetch('/api/addStudent', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formData)
-      });
+      const response = await axios.post(
+        "http://localhost:8080/api/admin/addStudents",
+        {
+          firstName: firstName,
+          middleName: middleName,
+          lastName: lastName,
+          email: email,
+          branch: branch,
+          campus: campus,
+          school: school,
+          password: password, // Include password in form data
+        }
+      );
 
-      if (response.ok) {
-        console.log('Student added successfully!');
-        // Reset form fields after successful submission
-        setName('');
-        setEmail('');
-        setBranch('');
-        setCampus('');
-      } else {
-        console.error('Failed to add student.');
-      }
+      console.log("Student added successfully!");
+      // Reset form fields after successful submission
+      setFirstName("");
+      setMiddleName("");
+      setLastName("");
+      setEmail("");
+      setBranch("");
+      setCampus("");
+      setSchool("");
+      setPassword(""); // Clear password field
     } catch (error) {
-      console.error('Error adding student:', error);
+      console.error("Error adding student:", error);
     }
   };
 
   return (
-    <div className="student-form-container" style={{ maxWidth: '400px', margin: 'auto', padding: '20px', border: '1px solid #ccc', borderRadius: '8px', boxShadow: '0 2px 4px rgba(0,0,0,0.1)' }}>
-      <h2 style={{ textAlign: 'center', marginBottom: '20px', color: '#333' }}>Add New Student</h2>
+    <div className="student-form-container" style={containerStyle}>
+      <h1 style={titleStyle}>Add New Student</h1>
       <form onSubmit={handleSubmit} className="student-form">
         <div className="form-group">
-          <label htmlFor="name" style={{ display: 'block', marginBottom: '5px', color: '#666' }}>Name:</label>
+          <label htmlFor="firstName" style={labelStyle}>
+            First Name:
+          </label>
           <input
             type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            style={{ width: '100%', padding: '8px', borderRadius: '4px', border: '1px solid #ccc', marginBottom: '15px' }}
+            id="firstName"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            style={inputStyle}
           />
         </div>
 
         <div className="form-group">
-          <label htmlFor="email" style={{ display: 'block', marginBottom: '5px', color: '#666' }}>Email:</label>
+          <label htmlFor="middleName" style={labelStyle}>
+            Middle Name:
+          </label>
+          <input
+            type="text"
+            id="middleName"
+            value={middleName}
+            onChange={(e) => setMiddleName(e.target.value)}
+            style={inputStyle}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="lastName" style={labelStyle}>
+            Last Name:
+          </label>
+          <input
+            type="text"
+            id="lastName"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            style={inputStyle}
+          />
+        </div>
+
+        {/* Other form fields */}
+        {/* Email, branch, campus, school, password */}
+        <div className="form-group">
+          <label htmlFor="email" style={labelStyle}>
+            Email:
+          </label>
           <input
             type="email"
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            style={{ width: '100%', padding: '8px', borderRadius: '4px', border: '1px solid #ccc', marginBottom: '15px' }}
+            style={inputStyle}
           />
         </div>
 
         <div className="form-group">
-          <label htmlFor="branch" style={{ display: 'block', marginBottom: '5px', color: '#666' }}>Branch:</label>
+          <label htmlFor="branch" style={labelStyle}>
+            Branch:
+          </label>
           <select
             id="branch"
             value={branch}
             onChange={(e) => setBranch(e.target.value)}
-            style={{ width: '100%', padding: '8px', borderRadius: '4px', border: '1px solid #ccc', marginBottom: '15px' }}
+            style={inputStyle}
           >
             <option value="">Select Branch</option>
             <option value="CSE">CSE</option>
@@ -85,12 +124,14 @@ const StudentForm = () => {
         </div>
 
         <div className="form-group">
-          <label htmlFor="campus" style={{ display: 'block', marginBottom: '5px', color: '#666' }}>Campus:</label>
+          <label htmlFor="campus" style={labelStyle}>
+            Campus:
+          </label>
           <select
             id="campus"
             value={campus}
             onChange={(e) => setCampus(e.target.value)}
-            style={{ width: '100%', padding: '8px', borderRadius: '4px', border: '1px solid #ccc', marginBottom: '15px' }}
+            style={inputStyle}
           >
             <option value="">Select Campus</option>
             <option value="BBSR">BBSR</option>
@@ -102,10 +143,81 @@ const StudentForm = () => {
           </select>
         </div>
 
-        <button type="submit" style={{ width: '100%', backgroundColor: '#007bff', color: '#fff', padding: '10px', borderRadius: '4px', border: 'none', cursor: 'pointer' }}>Add Student</button>
+        <div className="form-group">
+          <label htmlFor="school" style={labelStyle}>
+            School:
+          </label>
+          <select
+            type="text"
+            id="school"
+            value={school}
+            onChange={(e) => setSchool(e.target.value)}
+            style={inputStyle}
+          >
+            <option value="">Select School</option>
+            <option value="SOET">SOET</option>
+            <option value="SOM">SOM</option>
+            <option value="SOFS">SOFS</option>
+            <option value="Others">Others</option>
+          </select>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password" style={labelStyle}>
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={inputStyle}
+          />
+        </div>
+
+        <button type="submit" style={buttonStyle}>
+          Add Student
+        </button>
       </form>
     </div>
   );
+};
+
+const containerStyle = {
+  width: "50%",
+  margin: "auto",
+  border: "2px solid rgb(127 29 29)",
+  padding: "20px",
+  borderRadius: "10px",
+};
+
+const titleStyle = {
+  textAlign: "center",
+  color: "rgb(127 29 29)",
+};
+
+const labelStyle = {
+  display: "block",
+  marginBottom: "5px",
+  color: "#666",
+};
+
+const inputStyle = {
+  width: "100%",
+  padding: "8px",
+  borderRadius: "4px",
+  border: "1px solid #ccc",
+  marginBottom: "15px",
+};
+
+const buttonStyle = {
+  width: "100%",
+  padding: "10px",
+  backgroundColor: "rgb(127 29 29)",
+  color: "#fff",
+  border: "none",
+  borderRadius: "4px",
+  cursor: "pointer",
 };
 
 export default StudentForm;
